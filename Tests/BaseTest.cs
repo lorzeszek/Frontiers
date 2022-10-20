e287b233732c9ecce4547b25235744d77547c059
LukaszOrzeszek <lo@11x.eu>
2022-10-20 23:47:38 +0200

    Tests + BaseTest;

---
+++ Tests/BaseTest.cs
@@ -0,0 +1,35 @@
+﻿using FrontiersTask.Helpers;
+using FrontiersTask.Pages;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+
+namespace FrontiersTask
+{
+    public class BaseTest
+    {
+        protected IWebDriver _driver;
+        protected Browser _browser;
+        protected BaseTest(Browser browser)
+        {
+            _browser = browser;
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            _driver = new DriverFactory().Create(_browser);
+            _driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(10);
+            _driver.Manage().Window.Maximize();
+
+            MainPage mainPage = new MainPage(_driver);
+            mainPage.GoTo().CloseCookieNotification();
+        }
+
+        [TearDown]
+        public void Quit()
+        {
+            _driver.Quit();
+        }
+    }
+}
--- Tests/TestFrontiers.cs
+++ Tests/TestFrontiers.cs
@@ -1,61 +1,62 @@
 using FrontiersTask.Helpers;
+using FrontiersTask.Pages;
 using NUnit.Framework;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Support.UI;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace FrontiersTask
 {
-    [TestFixture(Browser.Chrome)]
-    public class TestFrontiers : TestBase
+    [TestFixture(Browser.Firefox)]
+    public class TestFrontiers : BaseTest
     {
         public TestFrontiers(Browser browser) : base (browser) {}
 
-        IWebElement constructionsDropdown => driver.FindElement(By.CssSelector("#lists-pages-select-container select"));
-        IReadOnlyCollection<IWebElement> buildingsTableRows => driver.FindElements(By.CssSelector("#buildingsTable tbody tr"));
-
         [Test]
-        //[TestCase("")]
-        public void CountTallest100CompletedBuildingsTest()
+        [TestCase("100")]
+        public void VerifyCompletedBuildingsCountTest(string expectedBuildingCount)
         {
-            wait.Until(driver => constructionsDropdown);
-            constructionsDropdown.SelectDropdownByValue("tallest100-completed");
+            var mainPage = new MainPage(_driver);
 
-            var expectedBuildingsCount = 100;
+            var buildingsCount = mainPage.SelectTallest100BuildingsFilter().GetBuildingsCount().ToString();
 
-            Assert.AreEqual(expectedBuildingsCount, buildingsTableRows.Count, string.Format("The list contains {0} instead of {1} elemets!", buildingsTableRows.Count, expectedBuildingsCount));
+            Assert.AreEqual(expectedBuildingCount, buildingsCount, string.Format("The list contains {0} instead of {1} elemets!", buildingsCount, expectedBuildingCount));
         }
 
         [Test]
-        //[TestCase("")]
-        public void LotteWorldTowerFloorsNumberTest()
+        [TestCase("Lotte World Tower", "Seoul", "123")]
+        public void VerifyBuildingFloorNumbers(string buildingName, string city, string expectedNumberOfFloors)
         {
-            wait.Until(driver => constructionsDropdown);
-            constructionsDropdown.SelectDropdownByValue("tallest100-completed");
+            var mainPage = new MainPage(_driver);
 
-            string expectedFloorsNumber = "123";
-            var tagetRowCells = buildingsTableRows.FirstOrDefault(x => x.FindElements(By.TagName("td")).Any(x => x.GetAttribute("data-order") == "Lotte World Tower"));
-            var floorsCellValue = tagetRowCells.FindElements(By.TagName("td")).FirstOrDefault(x => x.GetAttribute("class").Contains("forget")).Text;
-            
-            Assert.AreEqual(expectedFloorsNumber, floorsCellValue, string.Format("The Lotte World Tower building has {0} floors instead of {1}!", buildingsTableRows.Count, expectedFloorsNumber));
+            string targetBuildingFloorsCount = mainPage
+                .SelectTallest100BuildingsFilter()
+                .GetCityColumnIndex()
+                .GetBuildingNameColumnIndex()
+                .GetFloorsColumnIndex()
+                .CheckIfTargetBuildingExistsOnTheList(buildingName, city)
+                .GetBuildingFloorsNumber();
+
+            Assert.Multiple(() =>
+            {
+                Assert.Positive(mainPage.CityColumnIndex, "There is no column with label {0}!", "CITY");
+                Assert.Positive(mainPage.BuildingNameColumnIndex, "There is no column with label {0}!", "NAME");
+                Assert.Positive(mainPage.FloorsColumnIndex, "There is no column with label {0}!", "FLOORS");
+                Assert.IsTrue(mainPage.IsTargetBuildingOnTheList, "There building with name: {0}, in the city: {1} was not found on the list!", buildingName, city);
+                Assert.AreEqual(expectedNumberOfFloors, targetBuildingFloorsCount, string.Format("The Lotte World Tower building has {0} floors instead of {1}!", targetBuildingFloorsCount, expectedNumberOfFloors));
+            });
+
         }
 
         [Test]
-        //[TestCase("")]
-        public void MaxNumberOfFloorsBuildingTest()
+        public void GetMaxFloorsBuildingTest()
         {
-            wait.Until(driver => constructionsDropdown);
-            constructionsDropdown.SelectDropdownByValue("tallest100-completed");
+            var mainPage = new MainPage(_driver);
+            string maxFloorsBuildingName = mainPage.SelectTallest100BuildingsFilter()
+                .GetCityColumnIndex()
+                .GetBuildingNameColumnIndex()
+                .GetFloorsColumnIndex()
+                .GetBuildingWithMaxFloors();
 
-            var tagetRowCells = buildingsTableRows.FirstOrDefault(x => x.FindElements(By.TagName("td")).Max(x => x.GetAttribute("class").Contains("forget")));
-            //string expectedMaxFloorsBuilding = "Burj Khalifa";
-            var maxFloorsBuilding = tagetRowCells.FindElements(By.TagName("td")).FirstOrDefault(x => x.GetAttribute("class").Contains("building-hover")).Text;
-
-            //Assert.AreEqual(expectedMaxFloorsBuilding, maxFloorsBuilding, string.Format("The highest building is {0} instead of {1} !", buildingsTableRows.Count, expectedMaxFloorsBuilding));
-
-            Console.WriteLine(string.Format("The highest building is {0}.", maxFloorsBuilding));
+            Console.WriteLine(string.Format("The building with the most floors number is {0}.", maxFloorsBuildingName));
         }
     }
 }
