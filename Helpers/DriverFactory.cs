6144b5409131d5a7d18733c289a763628089adaa
LukaszOrzeszek <lo@11x.eu>
2022-10-20 23:47:17 +0200

    Helpers;

---
+++ Helpers/DriverFactory.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+
+namespace FrontiersTask.Helpers
+{
+    public class DriverFactory
+    {
+        private IWebDriver driver;
+        public IWebDriver Create(Browser browser)
+        {
+            switch (browser)
+            {
+                case Browser.Chrome:
+                    driver = new ChromeDriver();
+                    break;
+                case Browser.Firefox:
+                    driver = new FirefoxDriver();
+                    break;
+                default:
+                    throw new ArgumentException("Provided driver:" + browser + " is not supported. Available: chrome, firefox.");
+            }
+            return driver;
+        }
+    }
+}
--- Helpers/ExtensionMethods.cs
+++ Helpers/ExtensionMethods.cs
@@ -1,5 +1,7 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.ObjectModel;
 
 namespace FrontiersTask.Helpers
 {
@@ -8,6 +10,31 @@ namespace FrontiersTask.Helpers
         public static void SelectDropdownByValue(this IWebElement element, string value)
         {
             new SelectElement(element).SelectByValue(value);
+        }
+
+        public static IWebElement FindElement(this ISearchContext context, By by, int timeoutInSeconds)
+        {
+            if (timeoutInSeconds > 0)
+            {
+                DefaultWait<ISearchContext> wait = new DefaultWait<ISearchContext>(context);
+                wait.IgnoreExceptionTypes(typeof(NoSuchElementException));
+                wait.Timeout = TimeSpan.FromSeconds(timeoutInSeconds);
+                return wait.Until(c => c.FindElement(by));
+            }
+            return context.FindElement(by);
+
+        }
+
+        public static ReadOnlyCollection<IWebElement> FindElements(this ISearchContext context, By by, int timeoutInSeconds)
+        {
+            if (timeoutInSeconds > 0)
+            {
+                DefaultWait<ISearchContext> wait = new DefaultWait<ISearchContext>(context);
+                wait.IgnoreExceptionTypes(typeof(NoSuchElementException));
+                wait.Timeout = TimeSpan.FromSeconds(timeoutInSeconds);
+                return wait.Until(c => (c.FindElements(by).Count > 0) ? c.FindElements(by) : null);
+            }
+            return context.FindElements(by);
         }
     }
 }
